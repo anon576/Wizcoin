e785b0d2ebf55308c74546a781d665c749a4101f
Abhishek Bhoyar <abhibhoyar141@gmail.com>
2023-12-26 22:14:12 +0530

    Final Commit - Folder Structre

---
+++ node.py
@@ -0,0 +1,20 @@
+import asyncio
+from funcoin.blockchain import Blockchain
+from funcoin.connections import ConnectionPool
+from funcoin.peers import P2PProtocol
+from funcoin.server import Server
+
+blockchain = Blockchain()
+connectionPool = ConnectionPool()
+
+server = Server(blockchain,connectionPool,P2PProtocol)
+
+
+
+
+
+async def main():
+    #start the server
+    await server.listen()
+
+asyncio.run(main())
